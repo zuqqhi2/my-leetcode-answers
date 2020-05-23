bb251824ef3196db454608a77b1b4e63def092aa
zuqqhi2 <zuqqhi2@gmail.com>
2020-05-23 11:18:02 +0900

    Add 543

---
+++ 543-Diameter-of-Binary-Tree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+        self.ans = 1
+        def depth(node):
+            if not node: return 0
+            L = depth(node.left)
+            R = depth(node.right)
+            self.ans = max(self.ans, L+R+1)
+            return max(L, R) + 1
+
+        depth(root)
+        return self.ans - 1
+
+
+# Performance Result:
+#   Coding Time: -
+#   Time Complexity: O(N)
+#   Space Complexity: O(N)
+#   Runtime: 44 ms, faster than 73.15%
+#   Memory Usage: 16 MB, less than 44.83%
