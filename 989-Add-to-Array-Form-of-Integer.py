1cb4e5a799db00ab4f2a4c65e69969aa02109e80
zuqqhi2 <zuqqhi2@gmail.com>
2020-11-02 07:45:33 +0900

    Add 989

---
+++ 989-Add-to-Array-Form-of-Integer.py
@@ -0,0 +1,12 @@
+class Solution:
+    def addToArrayForm(self, A: List[int], K: int) -> List[int]:
+        res_str = str(int(''.join([str(A[i]) for i in range(len(A))]), 10) + K)
+        return [int(res_str[i], 10) for i in range(len(res_str))]
+
+
+# Performance Result:
+#   Coding Time: 00:05:40
+#   Time Complexity: O(N)
+#   Space Complexity: O(N)
+#   Runtime: 300 ms, faster than 65.62%
+#   Memory Usage: 15.1 MB, less than 91.87%
