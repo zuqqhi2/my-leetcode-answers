a55e0e884ca01bff6c1ffc597105dfae10ad0167
zuqqhi2 <zuqqhi2@gmail.com>
2021-07-10 10:24:49 +0900

    Update 1518

--- 1518-Water-Bottles.py
+++ 1518-Water-Bottles.py
@@ -1,10 +1,11 @@
 class Solution:
     def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
         res = numBottles
-        while numBottles // numExchange > 0:
-            res += numBottles // numExchange
-            numBottles = (numBottles // numExchange) + (
-                    numBottles % numExchange)
+        d = numBottles // numExchange
+        while d > 0:
+            res += d
+            numBottles = (d) + (numBottles % numExchange)
+            d = numBottles // numExchange
 
         return res
 
@@ -13,5 +14,5 @@ class Solution:
 #   Coding Time: 00:05:20
 #   Time Complexity: O(N)
 #   Space Complexity: O(1)
-#   Runtime: 52 ms, faster than 6.98%
-#   Memory Usage: 14 MB, less than 98.56%
+#   Runtime: 24 ms, faster than 94.35%
+#   Memory Usage: 14.2 MB, less than 70.18%
