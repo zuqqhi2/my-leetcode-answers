39a1ab9e10d623879b676fc7eb33c4801a8e744a
zuqqhi2 <zuqqhi2@gmail.com>
2021-07-19 09:38:40 +0900

    Add 1757

---
+++ 1757-Recyclable-and-Low-Fat-Products.sql
@@ -0,0 +1,8 @@
+SELECT product_id
+FROM Products
+WHERE low_fats = 'Y' AND recyclable = 'Y';
+
+-- Performance Result:
+--   Coding Time: 00:01:028
+--   Runtime: 723 ms, faster than 7.79%
+--   Memory Usage: 0B, less than 100.00%
