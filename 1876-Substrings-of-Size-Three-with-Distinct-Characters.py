e32ae612b1b03c764753a7e713cb4200210721bb
zuqqhi2 <zuqqhi2@gmail.com>
2022-04-11 09:49:24 +0900

    Add 1876

---
+++ 1876-Substrings-of-Size-Three-with-Distinct-Characters.py
@@ -0,0 +1,25 @@
+class Solution:
+    def countGoodSubstrings(self, s: str) -> int:
+        if len(s) < 3:
+            return 0
+
+        words = []
+        for i in range(len(s) - 2):
+            words.append(s[i:i + 3])
+
+        res = 0
+        for word in words:
+            if word[0] == word[1] or word[0] == word[2] or word[1] == word[2]:
+                continue
+            else:
+                res += 1
+
+        return res
+
+
+# Performance Result:
+#   Coding Time: 00:10:01
+#   Time Complexity: O(N log2 N)
+#   Space Complexity: O(1)
+#   Runtime: 64 ms, faster than 83.54%
+#   Memory Usage: 13.9 MB, less than 81.15%
