f01c5c5e80bb02dad2ce3d5a1d897f9001411010
zuqqhi2 <zuqqhi2@gmail.com>
2024-05-02 10:53:49 +0900

    Add 2235

---
+++ 2235-Add-Two-Integers.rs
@@ -0,0 +1,12 @@
+impl Solution {
+    pub fn sum(num1: i32, num2: i32) -> i32 {
+        num1 + num2
+    }
+}
+
+// Performance Result:
+//   Coding Time: 00:00:11
+//   Time Complexity: O(N)
+//   Space Complexity: O(N)
+//   Runtime: 0 ms, faster than 100.00%
+//   Memory Usage: 2.12 MB, less than 10.75%
