fe3ce6cce9f4e04fc856aa82895b9d4c8860b89b
zuqqhi2 <zuqqhi2@gmail.com>
2025-07-17 22:32:13 +0900

    Add 709 python

---
+++ 709-To-Lower-Case.py
@@ -0,0 +1,11 @@
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        return s.lower()
+
+
+# Performance Result:
+#   Coding Time: 00:00:12
+#   Time Complexity: O(N)
+#   Space Complexity: O(1)
+#   Runtime: 0 ms, faster than 100.00%
+#   Memory Usage: 17.84 MB, less than 23.15%
